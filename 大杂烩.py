4d579a9dd44a7668d4715b14c37b10ae19b4eb8d
NBChinese <112323554+NBChinese@users.noreply.github.com>
2022-08-28 19:55:25 +0800

    画图
    简单的python画图

---
+++ 大杂烩.py
@@ -0,0 +1,21 @@
+import turtle
+import time
+turtle.color('red')
+for i in range(3):
+    turtle.forward(100)
+    turtle.right(120)
+turtle.color('blue')
+for i in range(4):
+    turtle.forward(100)
+    turtle.right(90)
+turtle.color('green')
+for i in range(5):
+    turtle.forward(100)
+    turtle.right(72)
+turtle.color('purple')
+for i in range(6):
+    turtle.forward(100)
+    turtle.right(60)
+turtle.color('black')
+print('over')
+time.sleep(3)
